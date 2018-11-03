d267fbf7ba3acf729e644ddd6e47c855a51711be
masud001 <madhnagar@gmail.com>
2018-11-04 03:34:41 +0600

    added random ID genarator functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 // jquery library code here
 // =========================
 $(document).ready(function(){
+    // text container function "scroll-up" and "scroll-down" start here
+    // -----------------------------------------------------------------
+    // random id genarator function start (to detact scroll element dynamically)
+    // -------------------------------------------------------------------------
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+        for (var i = 0; i < 5; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+      }
+      
 
 });
 
