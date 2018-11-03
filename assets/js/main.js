f6a4efaae5f7b7a6e2c10b879bd97757d05b778e
masud001 <madhnagar@gmail.com>
2018-11-04 03:38:34 +0600

    added paragraph scroll-up function to scroll main-text scroll up-side

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,42 @@
 $(document).ready(function(){
     // text container function "scroll-up" and "scroll-down" start here
     // -----------------------------------------------------------------
-    // random id genarator function start (to detact scroll element dynamically)
-    // -------------------------------------------------------------------------
-    function makeid() {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    //  random id genarator function start (to detact scroll element dynamically)
+    //  glabally for thit wedding site
+    //  -------------------------------------------------------------------------
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        for (var i = 0; i < 5; i++)
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-        return text;
-      }
-      
+    return text;
+    }
+    // end ID genarator
+
+    // paragraph scroll-up "upore" function start here
+    // -----------------------------------------------
+function verticalSlideUp(){
+    $(".scroll-inner-container").each(function(){
+        let id = makeid();
+        var div = $(this);
+        div.stop();
+        $(this).attr("id",id);
+        var remHeight = div[0].scrollHeight - $(this).height();
+        var scrollableHeight = remHeight - div.scrollTop();
+        var pos = div.scrollTop();
+        var remainingTime = (remHeight - pos) * 100 / 5; //here 5 is a speed
+        div.animate({
+            scrollTop:remHeight
+        },{
+            duration: remainingTime,
+            easing: "linear",
+        });
+    });
+};
+
+
 
 });
 
