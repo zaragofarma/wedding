91d414bd204bfe1b778f5e38fafd9ca2f405fe2f
masud001 <madhnagar@gmail.com>
2018-11-04 03:45:16 +0600

    added mouse movement exucution function.when mouse hover over 10% top or 10% bottom then scroll up/down will be  start

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,9 +37,53 @@ function verticalSlideUp(){
         });
     });
 };
+    //  end scroll-up function
+    // paragraph scroll-down "neche" function start here
+    // -----------------------------------------------
+function verticalSlideDown(){
+    $(".scroll-inner-container").each(function(){
+        let id = makeid();
+        var div = $(this);
+        div.stop();
+        $(this).attr("id",id);
+        var remHeight = div[0].scrollHeight - $(this).height();
+        var scrollableHeight = remHeight - div.scrollTop();
+        var pos = div.scrollTop();
+        var remainingTime = (pos * 100) / 5; //here 5 is a speed
+
+        div.animate({
+            scrollTop:0
+        },{
+            duration: remainingTime,
+            easing: "linear",
+
+        });
+
+    });
+};
+    //  end scroll-down function
+
+    // mouse movement exucution start here .when mouse hover over 10% top or 10% bottom then scroll up/down start
+    // ----------------------------------------------------------------------------------------------------------
+var obj = $('.scroll-inner-container');
+var top, left, bottom, right;
+var excldH,objHeight,objWidth;
+// Get position of the div 'scroll-inner-container'
+    function getPos(obj) {
+    var offsets = obj.offset();
+    objHeight = obj.height();
+    objWidth = obj.width();
+    excldH = objHeight/3; //Caculating 10% height
+    top = offsets.top,
+    left = offsets.left,
+    bottom = top+objHeight,
+    right = left+objWidth
+};
+getPos(obj);
 
 
 
+    // document ready function end
 });
 
 // raw JavaScript code here
